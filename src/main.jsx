a515a53475429c5968f29aadf90895d5c549ab3a
chen4c <chen4393c@outlook.com>
2025-07-25 16:52:21 +0800

    Handling Not Found Errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
-} from 'react-router-dom';
-import './index.css';
-import Root from './routes/root.jsx';
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root.jsx";
+import ErrorPage from "./error-page.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
