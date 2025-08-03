906e1c8fe448f750a15ea9eaa6f5bf52e99b5c74
chen4c <chen4393c@outlook.com>
2025-08-03 21:37:08 +0800

    Get Submissions with Client Side Routing

--- src/routes/rootLoader.js
+++ src/routes/rootLoader.js
@@ -1,6 +1,9 @@
 import { getContacts } from "../contacts";
 
-export async function loader() {
-  const contacts = await getContacts();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  console.log('q:', q);
+  const contacts = await getContacts(q);
   return { contacts };
 }
